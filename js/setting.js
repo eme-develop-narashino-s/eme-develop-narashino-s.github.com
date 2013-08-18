6c5f172d09d064932bfefad1dde090338614129d
eme <m.eme.priv@live.jp>
2013-08-18 23:45:23 +0900

    api reload

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'narashino_2014';
+OpenSpending.identifier = 'narashino_2013';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
